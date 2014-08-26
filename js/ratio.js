0547d2270aa4710b8cfa13a24ebe0b3d00189368
Jonathan Delgado <alfa30@users.noreply.github.com>
2014-08-26 00:49:00 -0400

    Update ratio.js

--- js/ratio.js
+++ js/ratio.js
@@ -1,8 +1,11 @@
 $(window).ready(function(){
 	var nameRatio = "ratio";
 	$("."+nameRatio+" ."+nameRatio+"-item[data-model]").each(function(index,elem){
-		var model = $(elem).data('model');
-		$(elem).addClass('action');
-		$(elem).css("background-image","url("+$(elem).attr("src")+")");
+		$(elem).on('loadMode',function(event){
+			elem = $(this);
+			var model = $(elem).data('model');
+			$(elem).addClass('action');
+			$(elem).css("background-image","url("+$(elem).attr("src")+")");
+		})
 	});
 });
